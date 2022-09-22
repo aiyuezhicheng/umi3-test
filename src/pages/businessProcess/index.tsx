e0e2f3f3b5139e5a1600decb4fbdee1aae19f249
yuan.qiu <18332939190@139.com>
2022-09-22 21:58:24 +0800

    英文和繁體增加字段

--- src/pages/businessProcess/index.tsx
+++ src/pages/businessProcess/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
+import { Typography } from 'antd';
+import React from 'react';
 
 const BusinessProcess: React.FC = () => {
   return (
-    // <PageContainer title={false} >
+    <PageContainer title={false} content={<Typography.Text type="secondary">业务流程</Typography.Text>}>
       <div>业务流程</div>
-    // </PageContainer>
+     </PageContainer>
+    
   );
 };
 export default BusinessProcess;
